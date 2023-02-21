830efc5884bdc1d30032a594ba32e1799ff265d7
foruner <dankipchumba140@gmail.com>
2023-02-21 15:15:03 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 /**
  * main - print_putchar
  * is written by mbah
- * Return: always 0
+ * Return: Always 0
  */
 int main(void)
 {
@@ -15,5 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (o);
+	return (0);
 }
+
