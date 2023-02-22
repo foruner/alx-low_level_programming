315f65192e19fba44481e27b2fb0e252e3d82a8a
foruner <dankipchumba140@gmail.com>
2023-02-22 07:37:36 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return:always 0
+ */
+int main(void)
+{
+	r = _isalpha('H');
+	_putchar(r + '0');
+	r = _isalpha('o');
+	_putchar(r + '0');
+	r = _isalpha(108);
+	_putchar(r + '0');
+	r = _isalpha(';');
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
