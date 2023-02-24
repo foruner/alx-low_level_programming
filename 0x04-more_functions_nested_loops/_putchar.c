b06abab0ac83c8d5f8763dd3fffe72a2530b03e3
foruner <dankipchumba140@gmail.com>
2023-02-23 22:21:13 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -5,7 +5,7 @@
  * @c: The character to print
  *
  * Return: on success 1.
- * on error,-1 is returned,and erno is set appropriately.
+ * on error,-1 is returned,and errno is set appropriately.
  */
 int _putchar(char c)
 {
