ef267a28379617936ec199da34f0c97979db7992
foruner <dankipchumba140@gmail.com>
2023-02-27 12:14:17 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,6 +3,7 @@
  * prints2 - function should print only one character out of two
  * starting with the first one
  * @str: input
+ *
  * Return: print
  */
 void puts2(char *str)
@@ -24,6 +25,6 @@ void puts2(char *str)
 		{
 			_putchar(str[0]);
 		}
-		_putchar('\n');
-	}
+		}
+	_putchar('\n');
 }
