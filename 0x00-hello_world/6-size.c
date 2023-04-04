bf70eef1e4bcc6e647a76c44f5108ef54b5aca03
foruner <dankipchumba140@gmail.com>
2023-04-04 00:12:23 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * A program to print various sizes of computer types
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of an int: 4 byte(s)\n");
+	printf("size of an long int: 4 byte(s)\n");
+	printf("Size of a long long int: 8 byte(s)\n");
+	printf("Size of a float: 4 byte(s)\n");
+	return (0);
+}
+
