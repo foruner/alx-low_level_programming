4ed31787100dbc955da164f3315cc954f791b278
foruner <dankipchumba140@gmail.com>
2023-07-05 07:48:02 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,2 +1,13 @@
 #include<stdio.h>
-#include<main.h>
+#include"main.h"
+/**
+ * main - prints a string, followed by a new line.
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	puts("Puts with recursion");
+	return (0);
+}
