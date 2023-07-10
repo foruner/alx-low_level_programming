52573c783907f4c2a5b209343ce1c3360e3ccccc
foruner <dankipchumba140@gmail.com>
2023-07-09 19:19:31 -0700

    _memset task

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,11 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * _memset - to fill a block of memory with a specified value
+ * @s:It is a pointer to the memory block
+ * @b:It is the value to be set
+ * @n: It is the number of bytes to be filled in the memory block
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+}
