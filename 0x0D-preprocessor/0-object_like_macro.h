37b2b29f237fd339e103362fd90cafe4c7e251fe
foruner <dankipchumba140@gmail.com>
2023-07-17 14:04:17 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#define SIZE 1024
+/**
+ * SIZE - preprocessor variable
+ *
+ * Return: 0 (success)
+ */
+int main ()
+{
+	int z;
+	z = 98 + SIZE;
+	printf("%d\n", z);
+	return (0) 
+}
