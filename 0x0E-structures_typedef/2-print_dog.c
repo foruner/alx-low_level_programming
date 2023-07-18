656325c69aaa26930b15b4611dab3f1baea95aa4
foruner <dankipchumba140@gmail.com>
2023-07-18 13:43:03 -0700

    dog.h

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include"dog.h"
+void print_dog(struct dog *d);
+int main (void)
+{
+	*d name = poppy;
+	*d age = 3.5;
+	*d owner = bob;
+	printf("my name is %c, i'm %d years old and im owned by %c\n", *d name, *d age, *d owner);
+	return (0);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #include<stdio.h>
+#ifndef MY_DOG
+#define MY_DOG
 struct dog{
 	char * name;
 	float age;
 	char * owner;
 };
+#endif
